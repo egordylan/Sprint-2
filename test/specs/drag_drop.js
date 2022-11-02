eefbe017eb6c002a96f08b78125d06ab927c3292
egordylan <egordylan@gmail.com>
2022-11-02 20:08:31 +0100

    refact: add css selector

--- test/specs/drag_drop.js
+++ test/specs/drag_drop.js
@@ -10,27 +10,26 @@ describe('Drag and drop', async function () {
     context('Drag and drop element', async function () {
         it(`should successfully drag A to B`, async function () {
 
-            const elem = await $('//*[@id="column-a"]');
-            const target= await $('//*[@id="column-b"]');
-            // const elem = await $('#column-a');
-            // const target = await $('#column-b');
+            const elem = await $('#column-a');
+            const target = await $('#column-b');
 
-            
             // this test didn't work :(
             await elem.dragAndDrop(target);
             await browser.pause(3000);
+
             const elemHeader = await(await $('#column-a > header')).getText();
             console.log('A HEADER ::: ', elemHeader);
             const targetHeader = await(await $('#column-b > header')).getText();
             console.log('B HEADER ::: ', targetHeader);
+
             await expect(targetHeader).toHaveText('A');
             await expect(elemHeader).toHaveText('B');
         });
     });
 
-    // after('Close the window', async function () {
-    //     await browser.closeWindow();
-    // });
+    after('Close the window', async function () {
+        await browser.closeWindow();
+    });
 });
 
 // npx wdio run wdio.conf.js --spec drag_drop.js
